d42f60c65e6afdb917262919c61926bcd1997abe
Arseniy <gorbikovarseniy@gmail.com>
2026-02-18 21:25:55 +0200

    Homework 18.02.2026. Add display about me from API

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {linkApi} from "../utils/constants.js";
 
 const AboutMe = () => {
+    const [aboutMe, setAboutMe] = useState();
+    useEffect(() => {
+        fetch(`${linkApi}/v1/peoples/1`)
+            .then(response => response.json())
+            .then(data => setAboutMe(data))
+    }, []);
+    if (!aboutMe) {
+        return <div>Loading...</div>;
+    }
     return (
         <div>
-            About Me
+            <h1>{aboutMe.name}</h1>
+            <p>Gender: {aboutMe.gender}</p>
+            <p>Birth year: {aboutMe.birth_year}</p>
+            <p>Height: {aboutMe.height}</p>
+            <p>Mass: {aboutMe.mass}</p>
+            <p>Hair color: {aboutMe.hair_color}</p>
+            <p>Eye color: {aboutMe.eye_color}</p>
+            <p>Skin color: {aboutMe.skin_color}</p>
         </div>
     );
 };
